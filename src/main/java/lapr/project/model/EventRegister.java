200887823ca6b2041ea5a1f8c5f34ce784b6106e
Pedro <Pedro@172.18.154.23>
2017-06-05 16:14:18 +0100

    Recreating this class

---
+++ src/main/java/lapr/project/model/EventRegister.java
@@ -0,0 +1,17 @@
+package lapr.project.model;
+
+import java.util.List;
+
+public class EventRegister {
+
+    private List<Event> eventRegister;
+    
+    public EventRegister(){
+        
+    }
+    
+    public List<Event> getEventList(){
+        return eventRegister;
+    }
+    
+}
