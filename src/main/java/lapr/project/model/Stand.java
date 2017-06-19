5816c1af09310d185609dbda8c6e541fb202c63b
DESKTOP-3FEG0LC\Cerqueira <1161248@isep.ipp.pt>
2017-06-19 15:30:37 +0100

    changes in stand class

--- src/main/java/lapr/project/model/Stand.java
+++ src/main/java/lapr/project/model/Stand.java
@@ -11,51 +11,21 @@ package lapr.project.model;
  */
 public class Stand {
     
-    private typeStand type;
+    private int area;
     private boolean available=true; 
     
     /**
      * Enum that lists the possivel stands
      */
-    public enum typeStand{
-
-        SMALL_STAND("Small Stand", 10),
-        MEDIUM_STAND("Medium Stand", 20),
-        BIG_STAND("Big Stand", 40),
-        PREMIUM_STAND("Premium Stand", 80);
-
-        private int area;
-        private String name;
-
-        typeStand(String nome, int area) {
-            this.name = nome;
-            this.area = area;
-        }
-
-        public int getArea() {
-            return this.area;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-        
-        public void setArea(int area){
-            this.area=area;
-        }
-        
-        public void setName(String name){
-            this.name = name;
-        }
-    }
+    
 
         /**
          * Constructor
          *
          * @param type - type of stand
          */
-        public Stand(typeStand type) {
-            this.type = type;
+        public Stand(int area) {
+            this.area = area;
         }
 
         public Stand(){
@@ -67,7 +37,7 @@ public class Stand {
          * @return area (int)
          */
         public int getArea(){
-            return type.getArea();
+            return this.area;
         }
         
         /**
@@ -86,15 +56,9 @@ public class Stand {
             this.available=available;
         }
         
-        public String getName(){
-            return this.type.getName();
-        }
         
         public void setArea(int area){
-            this.type.area=area;
+            this.area=area;
         }
         
-        public void setName(String name){
-            this.type.name = name;
-        }
     }
