c3a0011933638aebadc6b6e84a9e425f8241cc58
DESKTOP-V10KUR4\Utilizador <1161069@isep.ipp.pt>
2017-06-21 00:10:31 +0100

    UC44 UI

--- src/main/java/lapr/project/ui/UC44_2_UI.java
+++ src/main/java/lapr/project/ui/UC44_2_UI.java
@@ -5,8 +5,10 @@
  */
 package lapr.project.ui;
 
+import java.util.List;
 import javax.swing.JOptionPane;
 import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
+import javax.swing.table.DefaultTableModel;
 import lapr.project.controller.UC44_Controller;
 import lapr.project.model.Event;
 import lapr.project.model.EventCenter;
@@ -17,24 +19,27 @@ import lapr.project.model.User;
  * @author Utilizador
  */
 public class UC44_2_UI extends javax.swing.JFrame {
-    
+
     static final long serialVersionUID = -3387516993124229948L;
     private UC44_Controller c;
-    private Event eventSelected;
     private EventCenter ec;
-    private String[] pickedList = new String[0];
-    private String[] topList;
+    private List<Event> events;
+    private String[] a;
 
     /**
      * Creates new form UC44_UI
      *
      * @param ec the instance of the event center
+     * @param events
+     * @param a
      *
      */
-    public UC44_2_UI(EventCenter ec) {
-    
+    public UC44_2_UI(EventCenter ec, List<Event> events, String[] a) {
+
         this.ec = ec;
         this.c = new UC44_Controller(ec);
+        this.events = events;
+        this.a = a;
         initComponents();
         this.setVisible(true);
         setLocationRelativeTo(null);
@@ -61,15 +66,14 @@ public class UC44_2_UI extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         Back = new javax.swing.JButton();
+        Inserir = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
+
             new Object [][] {
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null},
-                {null, null, null, null, null, null}
+
             },
             new String [] {
                 "Event", "Proportions of submissions accepted in n(i)  (n(i)>30", "Critical Value(zc)", "Significance level", "Observed value of test statistics ", "Decision over 50%(Yes or No)"
@@ -79,6 +83,13 @@ public class UC44_2_UI extends javax.swing.JFrame {
 
         Back.setText("Back");
 
+        Inserir.setText("Inserir");
+        Inserir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InserirActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -86,24 +97,68 @@ public class UC44_2_UI extends javax.swing.JFrame {
             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 1147, javax.swing.GroupLayout.PREFERRED_SIZE)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
+                .addComponent(Inserir)
+                .addGap(66, 66, 66)
                 .addComponent(Back)
                 .addGap(70, 70, 70))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
-                .addComponent(Back)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 104, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(Back)
+                    .addComponent(Inserir))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void InserirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InserirActionPerformed
+        DefaultTableModel val = (DefaultTableModel) Inserir.getModel();
+        double e;
+        String sl;
+        double cv;
+        double eve;
+        String decision;
+        for (int i = 0; i < events.size(); i++) {
+            e = events.get(i).getAcceptanceRate();
+            sl = getSignificanceLevel(events.get(i));
+            cv  = events.get(i).criticalValue(sl);
+            eve = events.get(i).getZUni();
+            decision = events.get(i).testAcceptanceRate50(sl);
+            val.addRow(new Object[]{e,sl,cv,eve,decision});
+        }
+    
+    
+        
+    }//GEN-LAST:event_InserirActionPerformed
+
+    
+    
+    
+    
+
+    public String getSignificanceLevel(Event e) {
+        String cv = "-1";
+        for (int i = 0; i < a.length; i++) {
+            String[] parts = a[i].split("-");
+            String part1 = parts[0];
+            String part2 = parts[1];
+            if (part1.equals(e.getTitle())) {
+                cv = parts[1];
+            }
+        }
+        return cv;
+
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Back;
+    private javax.swing.JButton Inserir;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
