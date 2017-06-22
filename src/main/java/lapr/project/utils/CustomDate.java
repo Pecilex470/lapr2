d15c4708708199e24251896e2f1304e534eb8a9f
Bruno Pinho <1161303@isep.ipp.pt>
2017-06-22 18:58:12 +0100

    Adding CustomDate options and validations to the UC1 and UC5, minor bug with UC5

--- src/main/java/lapr/project/utils/CustomDate.java
+++ src/main/java/lapr/project/utils/CustomDate.java
@@ -1,11 +1,10 @@
 package lapr.project.utils;
 
 import java.io.Serializable;
-import java.util.Calendar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+import javax.swing.JOptionPane;
 
 public class CustomDate implements Serializable {
 
@@ -31,6 +30,33 @@ public class CustomDate implements Serializable {
     }
 
     /**
+     * This method returns the date's day
+     *
+     * @return the day
+     */
+    public int getDay() {
+        return day;
+    }
+
+    /**
+     * This method returns the date's month
+     *
+     * @return the month
+     */
+    public int getMonth() {
+        return month;
+    }
+
+    /**
+     * This method returns the date's month
+     *
+     * @return
+     */
+    public int getYear() {
+        return year;
+    }
+
+    /**
      * Sets a specific date
      *
      * @param day the day to set
@@ -39,12 +65,26 @@ public class CustomDate implements Serializable {
      * @return returns the final object
      */
     public CustomDate setDate(int day, int month, int year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
+        if (0 < day && day <= daysMonth[month - 1]) {
+            if (0 < month && month <= 12) {
+                if (Integer.toString(year).length() == 4) {
+                    this.day = day;
+                    this.month = month;
+                    this.year = year;
+                    return CustomDate.this;
+                }
+            }
+        }
+        this.day = -1;
         return CustomDate.this;
     }
 
+    /**
+     * This method takes the local machine's time and creates a new Object with
+     * it
+     *
+     * @return returns the CustomDate object
+     */
     public static CustomDate getCurrentTime() {
         DateFormat df = new SimpleDateFormat("dd");
         DateFormat mf = new SimpleDateFormat("MM");
@@ -54,5 +94,20 @@ public class CustomDate implements Serializable {
         int m = Integer.parseInt(mf.format(calobj.getTime()));
         int y = Integer.parseInt(yf.format(calobj.getTime()));
         return new CustomDate(d, m, y);
+    }
+
+    /**
+     * This method checks if the current CustomDate object complies with the
+     * submission Period for a set Event
+     *
+     * @param minPeriod the beginning of the submission period
+     * @param maxPeriod the ending of the submission period
+     * @return returns if the condition is true or false
+     */
+    public boolean checkSubmissionPeriod(CustomDate minPeriod, CustomDate maxPeriod) {
+        if (day >= minPeriod.getDay() && day <= maxPeriod.getDay() && month >= minPeriod.getMonth() && month <= maxPeriod.getMonth() && year >= minPeriod.getYear() && year <= maxPeriod.getYear()) {
+            return true;
+        }
+        return false;
     }
 }
