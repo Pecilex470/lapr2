b6d81f57b654334414ef3ee0fb28597b9e095830
Bruno Pinho <1161303@isep.ipp.pt>
2017-06-23 05:55:51 +0100

    Creating tests for the encryption class

--- src/test/java/lapr/project/model/EncryptionTest.java
+++ src/test/java/lapr/project/model/EncryptionTest.java
@@ -1,197 +1,188 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package lapr.project.model;
-//
-//import org.junit.After;
-//import org.junit.AfterClass;
-//import org.junit.Before;
-//import org.junit.BeforeClass;
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-///**
-// *
-// * @author Pedro
-// */
-//public class EncryptionTest {
-//
-//    public EncryptionTest() {
-//    }
-//
-//    @BeforeClass
-//    public static void setUpClass() {
-//    }
-//
-//    @AfterClass
-//    public static void tearDownClass() {
-//    }
-//
-//    @Before
-//    public void setUp() {
-//    }
-//
-//    @After
-//    public void tearDown() {
-//    }
-//
-//    /**
-//     * Test of getShift method, of class Encryption.
-//     */
-//    @Test
-//    public void testGetShift() {
-//        System.out.println("getShift");
-//        Encryption instance = new Encryption();
-//        int expResult = 0;
-//        int result = instance.getShift();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setShift method, of class Encryption.
-//     */
-//    @Test
-//    public void testSetShift() {
-//        System.out.println("setShift");
-//        int shift = 0;
-//        Encryption instance = new Encryption();
-//        instance.setShift(shift);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getUser method, of class Encryption.
-//     */
-//    @Test
-//    public void testGetUser() {
-//        System.out.println("getUser");
-//        Encryption instance = new Encryption();
-//        User expResult = null;
-//        User result = instance.getUser();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setUser method, of class Encryption.
-//     */
-//    @Test
-//    public void testSetUser() {
-//        System.out.println("setUser");
-//        User user = null;
-//        Encryption instance = new Encryption();
-//        instance.setUser(user);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getKeyword method, of class Encryption.
-//     */
-//    @Test
-//    public void testGetKeyword() {
-//        System.out.println("getKeyword");
-//        Encryption instance = new Encryption();
-//        String expResult = "";
-//        String result = instance.getKeyword();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setKeyword method, of class Encryption.
-//     */
-//    @Test
-//    public void testSetKeyword() {
-//        System.out.println("setKeyword");
-//        String keyword = "";
-//        Encryption instance = new Encryption();
-//        instance.setKeyword(keyword);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of encryptPassword method, of class Encryption.
-//     */
-//    @Test
-//    public void testEncryptPassword() {
-//        System.out.println("encryptPassword");
-//        String password = "";
-//        int num = 0;
-//        String abc = "";
-//        String expResult = "";
-//        String result = Encryption.encryptPassword(password, num, abc);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of deEncryptPassword method, of class Encryption.
-//     */
-//    @Test
-//    public void testDeEncryptPassword() {
-//        System.out.println("deEncryptPassword");
-//        String password = "";
-//        int num = 0;
-//        String abc = "";
-//        String expResult = "";
-//        String result = Encryption.deEncryptPassword(password, num, abc);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of encryptData method, of class Encryption.
-//     */
-//    @Test
-//    public void testEncryptData() {
-//        System.out.println("encryptData");
-//        String param = "";
-//        String key = "";
-//        String expResult = "";
-//        String result = Encryption.encryptData(param, key);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of deEncryptData method, of class Encryption.
-//     */
-//    @Test
-//    public void testDeEncryptData() {
-//        System.out.println("deEncryptData");
-//        String param = "";
-//        String key = "";
-//        String expResult = "";
-//        String result = Encryption.deEncryptData(param, key);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of assemblePassword method, of class Encryption.
-//     */
-//    @Test
-//    public void testAssemblePassword() {
-//        System.out.println("assemblePassword");
-//        char[] chars = null;
-//        String expResult = "";
-//        String result = Encryption.assemblePassword(chars);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lapr.project.model;
+
+import lapr.project.controller.UC6_Controller;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Pedro
+ */
+public class EncryptionTest {
+
+    private User u;
+
+    public EncryptionTest() {
+
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+        String name = Encryption.encryptData("Pedro Miller Brandão Pinho", "zebras");
+        name = Encryption.encryptPassword(name, 6, Encryption.ABC);
+
+        String email = Encryption.encryptData("pedro.miller.pinho@gmail.com", "zebras");
+        email = Encryption.encryptPassword(email, 6, Encryption.ABC);
+
+        String password = Encryption.encryptPassword("Pm-10", 6, Encryption.ABC);
+
+        this.u = new User(name, email, "admin", password, true, true);
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getShift method, of class Encryption.
+     */
+    @Test
+    public void testGetShift() {
+        System.out.println("getShift");
+        Encryption instance = new Encryption(6, u, "zebras");
+        int expResult = 6;
+        int result = instance.getShift();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setShift method, of class Encryption.
+     */
+    @Test
+    public void testSetShift() {
+        System.out.println("setShift");
+        int shift = 0;
+        Encryption instance = new Encryption(6, u, "zebras");
+        instance.setShift(shift);
+    }
+
+    /**
+     * Test of getUser method, of class Encryption.
+     */
+    @Test
+    public void testGetUser() {
+        System.out.println("getUser");
+        Encryption instance = new Encryption(6, u, "zebras");
+        User expResult = u;
+        User result = instance.getUser();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setUser method, of class Encryption.
+     */
+    @Test
+    public void testSetUser() {
+        System.out.println("setUser");
+        User user = null;
+        Encryption instance = new Encryption();
+        instance.setUser(user);
+    }
+
+    /**
+     * Test of getKeyword method, of class Encryption.
+     */
+    @Test
+    public void testGetKeyword() {
+        System.out.println("getKeyword");
+        Encryption instance = new Encryption(6, u, "zebras");
+        String expResult = "zebras";
+        String result = instance.getKeyword();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of setKeyword method, of class Encryption.
+     */
+    @Test
+    public void testSetKeyword() {
+        System.out.println("setKeyword");
+        String keyword = "";
+        Encryption instance = new Encryption(6, u, "zebras");
+        instance.setKeyword(keyword);
+    }
+
+    /**
+     * Test of encryptPassword method, of class Encryption.
+     */
+    @Test
+    public void testEncryptPassword() {
+        System.out.println("encryptPassword");
+        String password = "Pm-10";
+        int num = 1;
+        String abc = Encryption.ABC;
+        String expResult = "Ol:Z9";
+        String result = Encryption.encryptPassword(password, num, abc);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of deEncryptPassword method, of class Encryption.
+     */
+    @Test
+    public void testDeEncryptPassword() {
+        System.out.println("deEncryptPassword");
+        String password = "Ol:Z9";
+        int num = 1;
+        String abc = Encryption.ABC;
+        String expResult = "Pm-10";
+        String result = Encryption.deEncryptPassword(password, num, abc);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of encryptData method, of class Encryption.
+     */
+    @Test
+    public void testEncryptData() {
+        System.out.println("encryptData");
+        String param = "Pedro Miller Brandão Pinho";
+        String key = "zebras";
+        String expResult = "PsaolzMfiisozBoekaãlzPfkdl";
+        String result = Encryption.encryptData(param, key);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of deEncryptData method, of class Encryption.
+     */
+    @Test
+    public void testDeEncryptData() {
+        System.out.println("deEncryptData");
+        String param = "PsaolzMfiisozBoekaãlzPfkdl";
+        String key = "zebras";
+        String expResult = "Pedro Miller Brandão Pinho";
+        String result = Encryption.deEncryptData(param, key);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of assemblePassword method, of class Encryption.
+     */
+    @Test
+    public void testAssemblePassword() {
+        System.out.println("assemblePassword");
+        char[] chars = {'a', 'b', 'c', 'd'};
+        String expResult = "abcd";
+        String result = Encryption.assemblePassword(chars);
+        assertEquals(expResult, result);
+    }
+
+}
---
+++ src/test/java/lapr/project/model/register/EncryptionRegisterTest.java
@@ -0,0 +1,98 @@
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package lapr.project.model.register;
+//
+//import java.util.List;
+//import lapr.project.model.Encryption;
+//import lapr.project.model.User;
+//import org.junit.After;
+//import org.junit.AfterClass;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+///**
+// *
+// * @author Pedro
+// */
+//public class EncryptionRegisterTest {
+//    
+//    public EncryptionRegisterTest() {
+//    }
+//    
+//    @BeforeClass
+//    public static void setUpClass() {
+//    }
+//    
+//    @AfterClass
+//    public static void tearDownClass() {
+//    }
+//    
+//    @Before
+//    public void setUp() {
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//    }
+//
+//    /**
+//     * Test of getEncryptionList method, of class EncryptionRegister.
+//     */
+//    @Test
+//    public void testGetEncryptionList() {
+//        System.out.println("getEncryptionList");
+//        EncryptionRegister instance = null;
+//        List<Encryption> expResult = null;
+//        List<Encryption> result = instance.getEncryptionList();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of setEncryptionList method, of class EncryptionRegister.
+//     */
+//    @Test
+//    public void testSetEncryptionList() {
+//        System.out.println("setEncryptionList");
+//        List<Encryption> encryptionList = null;
+//        EncryptionRegister instance = null;
+//        instance.setEncryptionList(encryptionList);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of addEncryption method, of class EncryptionRegister.
+//     */
+//    @Test
+//    public void testAddEncryption() {
+//        System.out.println("addEncryption");
+//        Encryption e = null;
+//        EncryptionRegister instance = null;
+//        instance.addEncryption(e);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//
+//    /**
+//     * Test of getEncryptionByUser method, of class EncryptionRegister.
+//     */
+//    @Test
+//    public void testGetEncryptionByUser() {
+//        System.out.println("getEncryptionByUser");
+//        User user = null;
+//        EncryptionRegister instance = null;
+//        Encryption expResult = null;
+//        Encryption result = instance.getEncryptionByUser(user);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
+//    
+//}
